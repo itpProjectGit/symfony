9dd4eaeacef2ee9994cb1e2ff5ee1be7cdc6fc73
itpProject <ssharevreetsing@students.latrobe.edu.au>
2014-10-16 11:09:23 +1100

    Revert "sourcecode"
    This reverts commit e130a360319c1ac3c8b665600c634d972183ca78.

--- symfony/contant.php
+++
@@ -1,9 +0,0 @@
-<?php 
-while ($obj = mysql_fetch_array($r))
-{ 
-?>
-        <div class="banner_heading"><?php echo $obj['title']; ?></div>
-        <div class="banner_p">
-        <p><?php echo $obj['contant']; ?></p>
-        </div>   
-<?php } mysql_close($dbhandle); ?>          
